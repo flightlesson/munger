6c8465eb077d5c282131dec7f5387df2e2f84238
Tim Singletary <tsingletary@hammers.com>
2016-04-20 18:21:39 -0400

    added a nonpublic .h file

--- lib-munge/munge.c
+++ lib-munge/munge.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "munge.h"
+#include "mungeinternal.h"
 
 char *munge(char *plain) {
   return MUNGE(plain);
--- lib-munge/munge.h
+++ lib-munge/munge.h
@@ -2,8 +2,6 @@
 #define MUNGE_H
 #include <ctype.h>
 
-#define MUNGE(x) ( *x = toupper(*x), x )
-
 char *munge(char* plain);
 
 #endif
